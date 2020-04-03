2fd8c177fe7f9c989b8d4ab7ec0aadccda2fd58c
jankalinowski233 <jan.kalinowski1998@gmail.com>
2020-04-03 19:10:11 +0100

    Initial commit

---
+++ Skrybawka/Assets/Scripts/SkrybawkaScripts/CameraFollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float cameraSpeed;
+    public float distance; // How far can we move away from camera center before moving the camera towards the player
+    public bool panCamera;
+
+    Vector3 cameraOffset;
+
+    private void Start() // Executed before LateUpdate()
+    {
+        cameraOffset = transform.position;
+    }
+
+    private void LateUpdate() // Executed at the end of the frame
+    {
+        if(panCamera == true)
+            PanCamera();
+        else
+            CenterCamera();
+    }
+
+    void PanCamera()
+    {
+        Vector3 playerPosition = player.position;
+        Vector3 direction = playerPosition - gameObject.transform.position;
+
+        float magnitude = Mathf.Sqrt((direction.x * direction.x) + (direction.y * direction.y));
+
+        if(magnitude >= distance)
+        {
+            gameObject.transform.Translate(new Vector2(direction.x, direction.y) * cameraSpeed * Time.deltaTime);
+        }
+    }
+
+    void CenterCamera()
+    {
+        // Place camera at X and Y of the player, remain at Z of the camera
+        gameObject.transform.position = new Vector3(player.position.x + cameraOffset.x, player.position.y + cameraOffset.y, transform.position.z) ;
+    }
+}
---
+++ Skrybawka/Assets/Scripts/SkrybawkaScripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    public float playerSpeed;
+    
+    [Header("Jump")]
+    [Space(15f)]
+    public float jumpForce;
+    bool isOnGround;
+    public Transform groundDetection;
+    public float detectionRadius;
+    public LayerMask groundMask;
+
+    public int maxJumps;
+    private int jumpCount = 0;
+
+    Animator anim;
+
+    private void Awake() // Runs first
+    {
+        // Grab components references
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        rb2d.velocity = new Vector2(x * playerSpeed, rb2d.velocity.y);
+
+        anim.SetFloat("Speed", x); // Tell animator to start (or stop) running
+
+        // TODO flipping character sprite
+    }
+
+    public void Jump()
+    {
+        // TODO jump animation
+
+        isOnGround = Physics2D.OverlapCircle(groundDetection.position, detectionRadius, groundMask);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpCount++;
+
+            if(jumpCount < maxJumps)
+            {
+                rb2d.AddForce(new Vector2(0.0f, jumpForce));
+            }            
+        }
+
+        if(isOnGround == true)
+        {
+            jumpCount = 0;
+        }
+    }
+}
